593756b4f143da9ddd977c66411cc6ae58d08a24
FaustinM <faustino40@outlook.fr>
2019-11-03 15:30:16 +0100

    Ajout des fichiers de base

---
+++ js/common.js
@@ -0,0 +1,8 @@
+function toggleNavBurger(){
+    document.getElementById("navbar-burger").classList.toggle("is-active");
+    document.getElementById("navbar-menu").classList.toggle("is-active");
+}
+
+function toggleDropdown() {
+    document.getElementById("dropdown-menu").classList.toggle("is-display");
+}
